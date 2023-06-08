49d73e0f5360a110e51553e169602d95d5074646
Anurag Shukla <anurag@Anurags-MacBook-Air.local>
2023-06-09 03:33:37 +0530

    frontend:{dashbord,productDetails,Cart}||backend:{Product APIProductDetals API}

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import connectDB from './config/db.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js';
+
+dotenv.config();
+
+connectDB(); // connect to mongodb
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('API is running ');
+});
+
+app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
