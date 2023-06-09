f43da03a1092c4ec048c847cdb1a39491bb0bb3b
Anurag Shukla <anurag@Anurags-MacBook-Air.local>
2023-06-09 11:55:21 +0530

    Backend:{User_Registration, Login, Logout, Email_&_Password_Validation, JWT, Bcrypt_&__Encryptic, Edit_Profile, Cookie_&_Clear_Cookie}

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './config/db.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -11,11 +13,15 @@ connectDB(); // connect to mongodb
 const app = express();
 const port = process.env.PORT || 5000;
 
+// cookie-parser is used
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
   res.send('API is running ');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
