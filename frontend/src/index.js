6372de49a5dbedb66e29cf83e89ccae02b48beb8
Anurag Shukla <anurag@Anurags-MacBook-Air.local>
2023-06-09 17:39:56 +0530

    frontend:{Login_Screen, Login_Functionality, User_Logout_Functionality, User_Registration_Screen, Registration_Functionality}

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,7 +14,9 @@ import './assets/styles/index.css';
 import reportWebVitals from './reportWebVitals';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
 import ProductScreen from './screens/ProductScreen';
+import RegisterScreen from './screens/RegisterScreen';
 import store from './store';
 
 const router = createBrowserRouter(
@@ -22,7 +24,9 @@ const router = createBrowserRouter(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
-      <Route path="cart" element={<CartScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
     </Route>
   )
 );
