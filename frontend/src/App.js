58763d2a731e27c973894ba35738c3639de6f799
Anurag Shukla <anurag@Anurags-MacBook-Air.local>
2023-06-10 18:34:36 +0530

    Readme.md_File || Scroll_Top_Problem_Resolve || Env.Example.File

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,11 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import ScrollToTop from './components/ScrollToTop';
 const App = () => {
   return (
     <>
+      <ScrollToTop />
       <ToastContainer />
       <Header />
       <main className="py-3">
